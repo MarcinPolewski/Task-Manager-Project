16e51cd31ad7636ada6212f9e66cf0b72851e8bd
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-09-17 16:02:04 +0200

    implemented loading css files of stages

--- src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/util/MemoryHandler.java
+++ src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/util/MemoryHandler.java
@@ -9,7 +9,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
 import java.net.URL;
@@ -22,13 +26,17 @@ public class MemoryHandler {
 
     public MemoryHandler(
             @Value("${scene.startScene.name}") String startSceneName,
-            @Value("classpath:${scene.startScene.path}") Resource startSceneResource
+            @Value("classpath:${scene.startScene.path}") Resource startSceneResource,
+            @Value("classpath:${scene.startScene.css.path}") Resource startSceneCSSResource,
+            @Value("classpath:${scene.default.css.path}") Resource defaultCSSResource
     )
     {
 
-        // adding scenes to scenes map
-        scenes.put(SceneId.START_SCENE, new SceneWrapper(SceneId.START_SCENE, startSceneName, startSceneResource));
+        // adding scenes to scenes map @set
+        scenes.put(SceneId.START_SCENE, new SceneWrapper(SceneId.START_SCENE, startSceneName, startSceneResource, startSceneCSSResource));
         // @TODO add all other scenes
+
+        SceneWrapper.setDefaultCSSResource(defaultCSSResource);
     }
 
     public SceneWrapper getSceneWrapper(SceneId sceneId) throws IndexOutOfBoundsException, IOException
@@ -36,15 +44,25 @@ public class MemoryHandler {
         SceneWrapper wrapper =  scenes.get(sceneId);
         if(wrapper == null)
             throw new IndexOutOfBoundsException("scene map does not have mapping for sceneId: " + sceneId);
+
+        // if class has not been loaded, load
         if(!wrapper.isFXMLLoaded())
         {
-            URL url = wrapper.getResource().getURL(); // thows IOExc
+            // load fxml file
+            URL url = wrapper.getFxmlResource().getURL(); // thows IOExc
             FXMLLoader loader = new FXMLLoader(url);
             Parent root = loader.load();        // throws IOException
             Scene scene = new Scene(root);
 
             wrapper.setController(loader.getController());
             wrapper.setScene(scene);
+
+            // load css file
+            String defaultCss = Files.readString(SceneWrapper.getDefaultCSSResource().getFile().toPath());
+            String scenesCss = Files.readString(wrapper.getSceneCSSResource().getFile().toPath());
+
+            scene.getStylesheets().add(defaultCss);
+            scene.getStylesheets().add(scenesCss);
         }
         return wrapper;
     }
--- src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/view/SceneWrapper.java
+++ src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/view/SceneWrapper.java
@@ -1,6 +1,7 @@
 package com.marcin.jacek.polewski.Task_Manager_Project.view;
 
 import javafx.scene.Scene;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.core.io.Resource;
@@ -9,22 +10,36 @@ import org.springframework.core.io.Resource;
 public class SceneWrapper {
 
 
+    @Getter(AccessLevel.NONE)
+    private static Resource defaultCSSResource;
     private final SceneId id;
     private final String name;
-    private final Resource resource;
+    private final Resource fxmlResource;
+    private final Resource sceneCSSResource;
     @Setter private Scene scene;
     @Setter private Object controller;
 
 
-    public SceneWrapper(SceneId id, String name, Resource resource)
+    public SceneWrapper(SceneId id, String name, Resource fxmlResource, Resource cssResource)
     {
         this.id = id;
         this.name = name;
-        this.resource = resource;
+        this.fxmlResource = fxmlResource;
+        this.sceneCSSResource = cssResource;
     }
 
     public boolean isFXMLLoaded()
     {
         return (scene!=null) && (controller!=null);
+    }
+
+    public static Resource getDefaultCSSResource()
+    {
+        return defaultCSSResource;
+    }
+
+    public static void setDefaultCSSResource(Resource res)
+    {
+        SceneWrapper.defaultCSSResource = res;
     }
 }
