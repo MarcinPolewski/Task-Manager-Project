d176865ec20ad1069b24f0dcce107ef83894fe72
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-09-19 08:20:58 +0200

    added languages folder path to application.properties

--- src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/JavaFXConfiguration.java
+++ src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/JavaFXConfiguration.java
@@ -15,14 +15,18 @@ public class JavaFXConfiguration {
     // @TODO add any necesary beans if needed(like Application, Parameters, HostServices..)
 
     private final String defaultLanguageTag;
-    JavaFXConfiguration(@Value("${language.default.tag}") String defaultLanguageTag)
+    private final String languagesFolderPath;
+    JavaFXConfiguration(
+            @Value("${language.default.tag}") String defaultLanguageTag,
+            @Value("${language.folder.path}") String languagesFolderPath)
     {
         this.defaultLanguageTag = defaultLanguageTag;
+        this.languagesFolderPath = languagesFolderPath;
     }
     @Bean
     public MessageSource messageSource() {
         ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:languages/messages");
+        messageSource.setBasename("classpath:" + languagesFolderPath);
         messageSource.setDefaultEncoding("UTF-8");
 
         Locale.setDefault(Locale.forLanguageTag(defaultLanguageTag));
