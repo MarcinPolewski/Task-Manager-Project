6836bd4e9e99b21efdc3a501457cfcea21d1b67d
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-09-28 10:26:25 +0200

    refactored code

--- src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/view/UIComponents/TasksOfTheDayPreview.java
+++ src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/view/UIComponents/TasksOfTheDayPreview.java
@@ -10,43 +10,51 @@ import javafx.scene.layout.HBox;
 import javafx.scene.shape.Line;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class TasksOfTheDayPreview extends VBox {
     private HBox topBarHBox = new HBox();
     private Button previousDay= new Button("<");
     private Button nextDay = new Button(">");
     private Label dayLabel = new Label();
+    private List<Line>hourLines = new ArrayList<>();
 
-    public TasksOfTheDayPreview(LocalDateTime currentTime, TaskManager taskManager)
+    private void setUpTopBar(LocalDateTime currentTime)
     {
-        super();
-        // top bar set up
         dayLabel.setText(currentTime.toString());
         topBarHBox.getChildren().setAll(previousDay, dayLabel, nextDay);
 
+        // add top bar to this component
+        this.getChildren().add(topBarHBox);
+    }
+
+    private void setUpHourScrollView(LocalDateTime currentTime, TaskManager taskManager)
+    {
         // day view setting
         VBox scrollVBox = new VBox();
         // Bind the VBox's width to the width of the ScrollPane's viewport
         scrollVBox.setFillWidth(true);
-        //scrollVBox.setMaxWidth();
 
+        //instantiate hour lines with labels assigned
         for(int hour=1; hour<= 24; ++hour)
         {
+
             Label hourLabel = new Label();
             hourLabel.setText(String.valueOf(hour));
 
             Line line = new Line();
-            // Bind the endX property of the Line to the width of the VBox
+            hourLines.add(line);
 
-            // @TODO import this value from css !!!
+            // @TODO import this value from css !!! Injection?
             double padding = 10.0;
+
+            // Bind the endX property of the Line to the width of the VBox
             line.endXProperty().bind(scrollVBox.widthProperty().subtract(20));
 
             VBox hourVBox = new VBox();
             hourVBox.getChildren().setAll(hourLabel, line);
             hourVBox.setPadding(new Insets(padding));
-            // @TODO FIX line above causes wrnog functionality,because window is stretched
-            // indefinitely to the left
 
             scrollVBox.getChildren().add(hourVBox);
         }
@@ -55,8 +63,14 @@ public class TasksOfTheDayPreview extends VBox {
         scroll.setFitToWidth(true);
         scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
 
-        this.getChildren().setAll(topBarHBox, scroll);
+        this.getChildren().add(scroll);
+    }
 
+    public TasksOfTheDayPreview(LocalDateTime currentTime, TaskManager taskManager)
+    {
+        super();
+        setUpTopBar(currentTime);
+        setUpHourScrollView( currentTime,  taskManager);
     }
 
 //    private void addTaskBlock(Task task)
