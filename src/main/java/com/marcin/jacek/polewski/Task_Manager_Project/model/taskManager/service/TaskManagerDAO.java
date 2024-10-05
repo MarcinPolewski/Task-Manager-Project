8b4295d4e6b3789882637c621a53bc66ccd368fd
Marcin Polewski <marcin.jacek.polewski@gmail.com>
2024-10-05 08:09:31 +0200

    deleted TaskManager DAO and Service (it's automatically loaded by User dao)

--- src/main/java/com/marcin/jacek/polewski/Task_Manager_Project/model/taskManager/service/TaskManagerDAO.java
+++
@@ -1,8 +0,0 @@
-package com.marcin.jacek.polewski.Task_Manager_Project.model.taskManager.service;
-
-import com.marcin.jacek.polewski.Task_Manager_Project.model.taskManager.TaskManager;
-import com.marcin.jacek.polewski.Task_Manager_Project.model.user.User;
-
-public interface TaskManagerDAO {
-    TaskManager find(User user);
-}
